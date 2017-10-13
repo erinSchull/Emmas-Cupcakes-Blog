90596765d096a344881d3666f4192fe99e3bbf8d
Erin Schull <erin.schull@gmail.com>
2017-10-13 11:49:39 -0600

    database and about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 
 
 class About extends Component{
+    constructor(){
+        super();
+
+        this.state = {
+            about: '',
+            title: ''
+        }
+    }
+
+    handleAboutChange(e){
+        this.setState({about: e.target.value})
+    }
+
     
+
     render(){
         return(
             <div>
-               About stuff
+                <input></input>
+               <input value = {this.state.about} onClick={(e) => this.handleAboutChange}></input>
             </div>
         )
     }
