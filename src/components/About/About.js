56d87c24d4676a0146867ddb80b1372130b3aa60
Erin Schull <erin.schull@gmail.com>
2018-01-31 13:30:27 -0700

    a little bit of styling on About page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,7 +10,9 @@ class About extends Component {
                 <h1>About Emma's Life</h1>
                 <h4>The Story</h4>
                 <div className='about-Text'>
+                    <br />
                     <p className='disclaimer' >Disclaimer: The following text is from the <a className='about-wix' href='https://elmitch101.wixsite.com/emmaslife' >Live Wix Site</a> with more blog posts. This site is just a practice site, the shop is not a real shop. </p>
+                    <br />
                     <h5>Life is life.</h5>
                     <p>I don't know how I would say that better. I just needed a place to explode my life so I give you: Emma's Life. <br />
                         Yes, my name is Emma. That isn't just some random name. I enjoy reading, writing, music, and TV. I enjoy baking and I might even start a "bake shop" sometime in the future, for those who live close enough to me... This just got awkward... <br />
