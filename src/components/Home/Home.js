e88b37f1dbe955b5b6a97fea5a738b458746e315
Erin Schull <erin.schull@gmail.com>
2018-02-01 17:21:57 -0700

    description added to landing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,7 +12,11 @@ class Home extends Component {
                         <h3 className='home-title' >DISCOVER EMMA'S LIFE</h3>
                         <br />
                         <p className="Home-intro">
-                            Come On In! (I don't bite)
+                            Come On In! (I don't bite) < br />
+                            <span>
+                            Check out my Blog, <br />
+                            See the cupcakes I've made, < br />
+                            And order some for yourself! </span>
                     </p>
                         <br />
                         <Link to='/previews' >
